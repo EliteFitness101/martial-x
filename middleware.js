8b6a74974742ec1fe530858ff5fe29a32befca64
EliteFitness101 <resonancefitnessng@gmail.com>
2026-04-23 06:20:39 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,17 +2,21 @@ import { NextResponse } from "next/server";
 
 export function middleware(req) {
   const role = req.cookies.get("role")?.value;
+  const sub = req.cookies.get("sub")?.value;
+
   const path = req.nextUrl.pathname;
 
-  const isAdminRoute = path.startsWith("/admin");
-
-  if (isAdminRoute && role !== "admin") {
+  if (path.startsWith("/admin") && role !== "admin") {
     return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  if (path.startsWith("/coach") && sub === "free") {
+    return NextResponse.redirect(new URL("/pricing", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/dashboard/:path*", "/admin/:path*", "/coach/:path*"],
 };
