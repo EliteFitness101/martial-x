0471456eaa594f1edb6abab3c675c22da5f6d3f8
EliteFitness101 <resonancefitnessng@gmail.com>
2026-04-23 07:19:55 +0100

    Update paymentService.js

--- services/paymentService.js
+++ services/paymentService.js
@@ -1,3 +1,10 @@
-export function verifyPayment() {
-  return true;
+import crypto from "crypto";
+
+export function verifyPaystack(req) {
+  const hash = crypto
+    .createHmac("sha512", process.env.PAYSTACK_SECRET)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+
+  return hash === req.headers["x-paystack-signature"];
 }
