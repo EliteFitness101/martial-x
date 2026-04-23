26d7cd4f81ab37acae72e14e983620e11738cf57
EliteFitness101 <resonancefitnessng@gmail.com>
2026-04-23 08:59:42 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>ResoFit SaaS is Live 🚀</h1>
+      <p>System deployed successfully</p>
+    </div>
+  );
+}
